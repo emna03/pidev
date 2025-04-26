ac45fd68fbab8d969a93d035d88d4b6bf1a07912
SiwarSlimi1 <siwar.slimi@esprit.tn>
2025-04-26 15:12:33 +0100

    ajouter  stat - debugging - adding features

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Documentadministratif;
+use App\Form\DocumentadministratifType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,8 +17,29 @@ class HomeController extends AbstractController
     }
 
     #[Route('/back', name: 'dashboard')]
-    public function index2(): Response
+    public function index2(EntityManagerInterface $entityManager): Response
     {
+         // Calculate counts for "Validé" and "Rejeté"
+    $countValide = $entityManager->getRepository(Documentadministratif::class)
+    ->createQueryBuilder('d')
+    ->select('COUNT(d.id)')
+    ->where('d.status = :status')
+    ->setParameter('status', 'Validé')
+    ->getQuery()
+    ->getSingleScalarResult();
+
+$countRejete = $entityManager->getRepository(Documentadministratif::class)
+    ->createQueryBuilder('d')
+    ->select('COUNT(d.id)')
+    ->where('d.status = :status')
+    ->setParameter('status', 'Rejeté')
+    ->getQuery()
+    ->getSingleScalarResult();
+
+return $this->render('home/index2.html.twig', [
+    'count_valide' => $countValide, // Pass counts to the template
+    'count_rejete' => $countRejete,
+]);
         return $this->render('home/index2.html.twig');
     }
     
